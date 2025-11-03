7e1fe2d5450df78c72782bf6ed4e6501ff1b942e
kvmilos <kamiltomaszek@icloud.com>
2025-11-03 16:53:09 +0100

    clean the repo

--- ud_converter/uvd.ps1
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/UniversalDependencies/tools.git udtools
-Remove-Item -Recurse -Force validate-data\*
-Copy-Item -Recurse udtools\data\* validate-data\
-Remove-Item -Recurse -Force udtools
